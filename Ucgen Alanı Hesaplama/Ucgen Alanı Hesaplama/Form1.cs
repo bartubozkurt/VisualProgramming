e27f433cf755671c7d8b5733faac942703ee97fa
Bartu Bozkurt <45657409+bartubozkurt@users.noreply.github.com>
2019-11-19 14:28:51 +0300

    Add files via upload

---
+++ Ucgen Alanı Hesaplama/Ucgen Alanı Hesaplama/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ucgen_Alanı_Hesaplama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int taban, yukseklik, alan=0;
+            taban = Convert.ToInt32(textBox1.Text);
+            yukseklik = Convert.ToInt32(textBox2.Text);
+            alan = (taban * yukseklik) / 2;
+            textBox3.Text = Convert.ToString(alan);
+        }
+    }
+}
---
+++ Windows Form Yazılı Ortalaması Hesaplama/Windows Form Yazılı Ortalaması Hesaplama/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Windows_Form_Yazılı_Ortalaması_Hesaplama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double v1, v2, f, ort;
+            v1 = Convert.ToDouble(textBox1.Text);
+            v2 = Convert.ToDouble(textBox2.Text);
+            f = Convert.ToDouble(textBox3.Text);
+            ort = (v1 + v2 + f) / 3;
+            textBox4.Text = Convert.ToString(ort);
+            if(ort >= 50)
+            {
+                textBox5.Text = "GEÇTİNİZ ! :)";
+                textBox5.ForeColor = Color.Green;
+            }
+            else
+            {
+                textBox5.Text = "KALDINIZ ! :(";
+                textBox5.ForeColor = Color.Red;
+            }
+        }
+    }
+}
