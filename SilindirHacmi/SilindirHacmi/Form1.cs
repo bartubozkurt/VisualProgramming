371f5cd0bfbf5f06c9ba64596500657cedfa6d9c
Bartu Bozkurt <45657409+bartubozkurt@users.noreply.github.com>
2019-11-19 14:53:05 +0300

    Add files via upload

---
+++ SilindirHacmi/SilindirHacmi/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SilindirHacmi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double r = Convert.ToDouble(textBox1.Text);
+            double h = Convert.ToDouble(textBox2.Text);
+            double hacim = Math.PI * r * r * h;
+            textBox3.Text = Convert.ToString(hacim);
+        }
+    }
+}
